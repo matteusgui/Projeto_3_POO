e9b7f87385a50fb284c3e13c9cac27acbbe4e7c9
dinizvt <vitordiniz2010@hotmail.com>
2019-12-08 02:41:54 -0300

    Agora o jogo roda em uma interface :) 	Alteraçoes sugeridas: 		*Criaçao de menus (classe menus? n sei) 		*Em caso de erro na leitura de algum arquivo de imagem criar shapes Circulo e Bola 		*Algum graphic designer salva nois 		*Apresentaçao dos resultados do jogo 		*Exibir possivel jogada meio transparente quando colocar o mouse em cima da posicao 		*Alterar o tamanho das imagens de acordo com o tamanho do jogo ou chegar a um consenso sobre o tamanho do jogo 		*Exibir "vez de {player}..." 		*Implementar o modo Super Jogo da Velha 		*Criar modo single

--- Game/Classes/Game.cpp
+++ Game/Classes/Game.cpp
@@ -1,24 +1,90 @@
 #include "Game.h"
 
+const sf::Color white(255,255,255);
+const sf::Color black(0, 0, 0);
+
 Game::Game(Player *p1, Player *p2)
 {
     this->p1 = p1;
     this->p2 = p2;
     this->active = p1;
-    for(int i = 0; i < SIZE; i++)
+    for(int i = 0; i < 3; i++)
     {
-        for(int j = 0; j < SIZE; j++)
+        for(int j = 0; j < 3; j++)
         {
             matrix[i][j] = 0;
         }
     }
 }
 
+void Game::drawGrade()
+{
+    sf::RectangleShape lineH(sf::Vector2f(size, 5));
+    lineH.setFillColor(black);
+    lineH.setPosition(position.x, position.y + size/3);
+    window->draw(lineH);
+    lineH.setPosition(position.x, position.y + size*2/3);
+    window->draw(lineH);
+    
+    sf::RectangleShape lineV(sf::Vector2f(5, size));
+    lineV.setFillColor(black);
+    lineV.setPosition(position.x + (size/3), position.y);
+    window->draw(lineV);
+    lineV.setPosition(position.x+(size*2/3), position.y);
+    window->draw(lineV);
+}
+
+void Game::drawPlays()
+{
+    sf::Texture oTexture;
+    oTexture.loadFromImage(oImage);
+    
+    sf::Texture xTexture;
+    xTexture.loadFromImage(xImage);
+
+    float radius = size/12;
+    float thickness = radius/10;
+
+    sf::Sprite circle;
+    circle.setTexture(oTexture);
+
+    sf::Sprite xshp;
+    xshp.setTexture(xTexture);
+
+    for(int i = 0; i < 3; i++)
+        {
+            for(int j = 0; j < 3; j++)
+            {
+                if(matrix[i][j])
+                {
+                    switch (matrix[i][j]->getSymbol())
+                    {
+                        case 'O':
+                        {
+                            circle.setPosition(0.5*size/6 + position.x, 0.5*size/6 + position.y);
+                            circle.move(j*size/3, i*size/3);
+                            window->draw(circle); 
+                        }
+                        break;
+                        case 'X':
+                        {
+                            xshp.setPosition(0.5*size/6 + position.x, 0.5*size/6 + position.y);
+                            xshp.move(j*size/3, i*size/3);
+                            window->draw(xshp); 
+                            
+                        }
+                        
+                    }
+                }
+            }
+        }
+}
+
 bool Game::isFull()
 {
-    for(int i = 0; i < SIZE; i++)
+    for(int i = 0; i < 3; i++)
     {
-        for(int j = 0; j < SIZE; j++)
+        for(int j = 0; j < 3; j++)
         {
             if(!matrix[i][j])
                 return false;
@@ -27,49 +93,44 @@ bool Game::isFull()
     return true;
 }
 
-Player* Game::getActive()
-{
-    return this->active;
-}
-
 Player* Game::winner()
 {
     //verify main diagonal
-    for(int i = 1; i < SIZE && matrix[i][i]; i++)
+    for(int i = 1; i < 3 && matrix[i][i]; i++)
     {
         if(matrix[i][i] != matrix[i-1][i-1])
             break;
-        if(i == SIZE-1)
+        if(i == 3-1)
             return matrix[i][i]; 
     }
 
     //verify antidiagonal
-    for (int i = 1; i < SIZE && matrix[i][SIZE-1-i]; i++) 
+    for (int i = 1; i < 3 && matrix[i][3-1-i]; i++) 
     {
-        if(matrix[i][SIZE-1-i] != matrix[i-1][SIZE-i])
+        if(matrix[i][3-1-i] != matrix[i-1][3-i])
             break;
-        if(i == SIZE-1) 
-            return matrix[i][SIZE-1-i];
+        if(i == 3-1) 
+            return matrix[i][3-1-i];
     }
     //verify lines
-    for (int i = 0; i < SIZE; i++)
+    for (int i = 0; i < 3; i++)
     {
-        for(int j = 1; j < SIZE && matrix[i][j]; j++)
+        for(int j = 1; j < 3 && matrix[i][j]; j++)
         {
             if(matrix[i][j] != matrix[i][j-1])
                 break;
-            if(j == SIZE-1) 
+            if(j == 3-1) 
                 return matrix[i][j];
         }
     }
     //verify columns
-    for (int j = 0; j < SIZE; j++)
+    for (int j = 0; j < 3; j++)
     {
-        for(int i = 1; i < SIZE && matrix[i][j]; i++)
+        for(int i = 1; i < 3 && matrix[i][j]; i++)
         {
             if(matrix[i][j] != matrix[i-1][j])
                 break;
-            if(i == SIZE-1)
+            if(i == 3-1)
                 return matrix[i][j];
         }
     }
@@ -78,9 +139,9 @@ Player* Game::winner()
         
 bool Game::play(int lin, int col)
 {
-    if(lin >= SIZE || lin < 0) 
+    if(lin >= 3 || lin < 0) 
         return false;
-    else if (col >= SIZE || col < 0) 
+    else if (col >= 3 || col < 0) 
         return false;
     else if (matrix[lin][col]) 
         return false;
@@ -97,9 +158,9 @@ bool Game::play(int lin, int col)
 
 void Game::print()
 {
-    for(int i = 0; i < SIZE; i++)
+    for(int i = 0; i < 3; i++)
     {
-        for(int j = 0; j < SIZE; j++)
+        for(int j = 0; j < 3; j++)
         {
             if(matrix[i][j])
                 cout << matrix[i][j]->getSymbol(); 
